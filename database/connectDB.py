5b4ff1588be01045e54a678ec625840f24a28476
Easy <easyconceptsrl@gmail.com>
2026-07-12 16:07:51 +0100

    update connectDB

--- database/connectDB.py
+++ database/connectDB.py
@@ -1,7 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///data/tecno_database.db"
+#DATABASE_URL = "sqlite:///data/tecno_database.db"
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DB_PATH = os.path.join(BASE_DIR, "data", "tecno_database.db")
+
+DATABASE_URL = f"sqlite:///{DB_PATH}"
 # Exemple MySQL : "mysql+pymysql://user:password@localhost/dbname"
 # DATABASE_URL = "mysql+pymysql://user:password@host/database"  # Pour MySQL, remplacez 'user', 'password', 'host' et 'database' par vos informations de connexion.
 # DATABASE_URL = "postgresql+psycopg2://user:password@host/database"  # Pour PostgreSQL, remplacez 'user', 'password', 'host' et 'database' par vos informations de connexion.
